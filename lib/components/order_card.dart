34033b64abd463077035335a1c75efe6eaf18d52
FanonX Rogers <fanonxdevelops@gmail.com>
2019-08-11 20:46:33 -0400

    Orders page: complete UI setup for orders page. Needs to handle the data dynamically now.

---
+++ lib/components/order_card.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class OrderCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              height: 75.0,
+              width: 45.0,
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 2.0,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Column(
+                children: <Widget>[
+                  InkWell(
+                    onTap: () {
+                      // increase the quantity of item
+                    },
+                    child: Icon(Icons.keyboard_arrow_up),
+                  ),
+                  Text(
+                    "0",
+                    style: TextStyle(fontSize: 18.0, color: Colors.green),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      // decrease quantity of item
+                    },
+                    child: Icon(Icons.keyboard_arrow_down),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/ediables/brownie-ediable.png"),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(30.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 5.0,
+                    offset: Offset(0.0, 5.0),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 20.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "Super Kush",
+                  style: TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  "Price: 43",
+                  style: TextStyle(color: Colors.green, fontSize: 15.0),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Container(
+                  height: 25.0,
+                  width: 120.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.only(right: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text(
+                              "Dust",
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            SizedBox(
+                              width: 7.0,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                // add/get rid of add ons
+                              },
+                              child: Text(
+                                "x",
+                                style: TextStyle(
+                                    color: Colors.red,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: (){
+                // remove item from the cart
+              },
+              child: Icon(
+                Icons.cancel,
+                color: Colors.red,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nimbus/components/order_card.dart';
 
 class OrderScreen extends StatefulWidget {
   @override
@@ -8,8 +9,93 @@ class OrderScreen extends StatefulWidget {
 class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Order Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cart", style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0,),
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          OrderCard(),
+          OrderCard(),
+          OrderCard(),
+          OrderCard(),
+          OrderCard(),
+          OrderCard(),
+        ],
+      ),
+      bottomNavigationBar: _buildTotalContainer(),
+    );
+  }
+
+  Widget _buildTotalContainer() {
+    return Container(
+      height: 240.0,
+      margin: EdgeInsets.only(top: 20.0),
+      padding: EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: <Widget>[
+//          ListTile(
+//            leading: Text('Cart Total:', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+//            trailing: Text("86", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),)
+//          ),
+//          ListTile(
+//              leading: Text('Discount:', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+//              trailing: Text("86", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),)
+//          ),
+//          ListTile(
+//              leading: Text('Cart Total', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+//              trailing: Text("86", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),)
+//          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text('Cart Total:', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+            Text("86", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),),
+          ],
+        ),
+          SizedBox(height: 10.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Discount:', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+              Text("3.0", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),),
+            ],
+          ),
+          SizedBox(height: 10.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Tax:', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+              Text("0.5", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),),
+            ],
+          ),
+          Divider(height: 40.0,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Sub Total', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+              Text("83.5", style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.green),),
+            ],
+          ),
+          SizedBox(height: 20.0,),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(60.0),
+            ),
+            child: Center(
+              child: Text("Procced to Checkout", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18.0),),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
